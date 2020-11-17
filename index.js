9a2b8c93f142328f9660dc33849d55a1777566d3
Muhammad Moiz Tahir <moizchaudhary35@gmail.com>
2020-11-17 09:53:59 +0500

    add pupiter and scrap data from cookie storage

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
 const app = express();
 const port = 3000;
 
+const tokenScrapper = async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);
+
+    await page.goto(
+      "https://www.tangoenergy.com/energyplans?marketsegment=Home&postcode=3003"
+    );
+
+    const returnedCookie = await page.cookies();
+
+    const token = returnedCookie.filter((data) => {
+      return data.name === "token";
+    });
+
+    return token[0].value;
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
 app.get("/", async (req, res) => {
   try {
+    let tokenResponse = {};
+
+    const recievedToken = await tokenScrapper();
+
+    tokenResponse = { token: recievedToken };
+
     const { data } = await axios.get(
       "https://www.tangoenergy.com/energyplans?marketsegment=Home&postcode=3003"
     );
@@ -15,9 +44,9 @@ app.get("/", async (req, res) => {
 
     const csrfToken = cherioLoader("meta[name='csrf-token']").attr("content");
 
-    console.log(csrfToken);
+    tokenResponse = { ...tokenResponse, "csrf-token": csrfToken };
 
-    res.status(200).send(csrfToken);
+    res.status(200).send(tokenResponse);
   } catch (err) {
     res.status(501).send(err);
   }
